a2f582fcd245afa6a0d42b20a6b8c10c7f558dd5
Jeca <smsacha2013@yandex.ru>
2025-10-16 21:04:08 +0700

    workers router (add in your business)

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from src.routers import user
 from src.routers import business
+from src.routers import worker
 
 
 app = FastAPI(title="КУ")
 
 app.include_router(user.router)
 app.include_router(business.router)
+app.include_router(worker.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
